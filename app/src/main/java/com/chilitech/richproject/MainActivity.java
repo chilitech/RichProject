7355d89c215ed81035396945d3f204e7bb278fc8
Kael <324904997@qq.com>
2021-08-14 17:55:19 +0800

    init

---
+++ app/src/main/java/com/chilitech/richproject/MainActivity.java
@@ -0,0 +1,20 @@
+package com.chilitech.richproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rv = this.findViewById(R.id.rv);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,5);
+    }
+}
---
+++ app/src/main/java/com/chilitech/richproject/databinding/entity/DataBindingDemoEntity.java
@@ -0,0 +1,5 @@
+package com.chilitech.richproject.databinding.entity;
+
+public class DataBindingDemoEntity {
+    private String name;
+}
